618387858fae7b164e9a4e72cf05c67a6080de21
Emmah Kimari <emalindahkimari@gmail.com>
2022-04-13 15:14:34 +0300

    create marketplace smart contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,47 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    const NFTMarket = await ethers.getContractFactory("NFTMarket");
+    const Market = await NFTMarket.deploy();
+    await Market.deployed();
+    const marketAdress = Market.address;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(marketAdress);
+    await nft.deployed();
+    const nftAddress = nft.address;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let listingPrice = await Market.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('100', 'ether');
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await nft.createToken("mytokelocation.com");
+    await nft.createToken("mytokelocation2.com");
+
+    await Market.createMarketItem(nftAddress, 1, auctionPrice, {value: listingPrice});
+    await Market.createMarketItem(nftAddress, 2, auctionPrice, {value: listingPrice});
+
+    const [_, buyerAddress] = await ethers.getSigners();
+
+    await Market.connect(buyerAddress).createMarketSale(nftAddress, 1, {value: auctionPrice});
+
+    let items = await Market.fetchMarketItems();
+
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId);
+      let item = {
+        price : i.price.toString(),
+        tokenId : i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        sold: i.sold,
+        tokenUri
+      }
+      return item;
+    }))
+    console.log('Unsold items', items);
   });
 });
